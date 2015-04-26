d29f33b7c073accd3b7b459280e90be43aed7717
Tomas Havlik <me@tomires.eu>
2015-04-26 17:01:02 +0200

    Topic overview
    Created a more elaborate topic overview page, fixed scrolling, added a
    progress bar, made major changes to the topics JSON and added a kitten.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
   var app = angular.module('bunsho', [ ]);
 
-  app.controller('StateController', function(){
+  app.controller('StateController', ['$window', function($window){
     this.state = 0;
     /* 0    course list
        1    prelesson page
@@ -11,23 +11,42 @@
 
     this.lesson = 0;
     this.nofquestions = 0;
+    this.questionsanswered = 0;
+    this.barwidth = 0;
 
     this.isState = function(xstate){
       return this.state === xstate;
     };
 
     this.setLesson = function(xlesson){
+      window.scrollTo(0, 0);
       this.lesson = xlesson;
       this.state = 1;
     };
 
     this.nextState = function(){
+      window.scrollTo(0, 0);
+      this.questionsanswered = 0;
+      this.barwidth = 0;
       if(this.state === 3)
         this.state = 0;
       else
         this.state++;
     };
-  });
+
+    this.nextQuestion = function(){
+
+      /* validation and shit */
+
+      $window.scrollTo(0, 0);
+      this.questionsanswered++;
+      this.barwidth = 100 * (this.questionsanswered / this.nofquestions);
+      if(this.questionsanswered === this.nofquestions){
+        this.barwidth = 0;
+        this.nextState();
+      }
+    }
+  }]);
 
   app.controller('TopicsController', ['$http', function($http){
     var bunsho = this;
@@ -45,9 +64,14 @@
       else
 		    this.item = xitem;
 	  };
+
 	  this.isSelected = function(xitem){
 		  return this.item === xitem;
 	  };
+
+    this.isAvailable = function(xid){
+      return bunsho.products[xid-1].enabled;
+    }
 
   }]);
 
