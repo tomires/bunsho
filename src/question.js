3683e0ec4a2bbe35aeef1a495ce513566ed36833
Tomas Havlik <me@tomires.eu>
2015-05-06 14:33:45 +0200

    Basic hintwork
    Provides some basic hints at how the hints system is going to work.
    
    Hint: it's not yet finished.

--- src/question.js
+++ src/question.js
@@ -1,41 +1,53 @@
+/* handles work with individual questions */
 angular.module('bunsho').controller('QuestionController', ['$http', '$scope', function($http, $scope){
-  var bunsho = this;
-  this.products = [];
-  this.alreadyAnswered = [];
-  this.currentQuestion = 0;
-  this.userAnswer = '';
-  this.results = [];
-  this.noCorrect;
-  this.noIncorrect;
-  $scope.parsedQuestion = '<b>ullshit</b>';
-  $scope.resultsMeter = ' ';
+  var bunsho = this; /* provides context to the HTTP getter */
+  this.products = []; /* an array containing all the questions */
+  this.alreadyAnswered = []; /* questions that have already been answered */
+  this.currentQuestion = 0; /* number of the current question in the current lesson */
+  this.userAnswer = ''; /* answer provided by the user, dynamically updated */
+  this.results = []; /* an array containing question results in the current session */
+  this.noCorrect; /* number of correct answers in the current session */
+  this.noIncorrect; /* number of incorrect answers in the current session */
+  this.revisionList = [];
+  $scope.parsedQuestion = '<b>Connection error</b>'; /* question as it shows up on the page */
+  $scope.resultsMeter = ' '; /* results display that shows up on the page */
+  $scope.revisionWords = '';
 
+  /* gets data from an appropriate json file */
   this.loadQuestions = function(lesson){
     $http.get('/json/' + lesson + '.json').success(function(data){
       bunsho.products = data;
       bunsho.results = [];
+      bunsho.revisionList = [];
       $scope.resultsMeter = ' ';
+      $scope.revisionWords = '';
       bunsho.randomQuestion();
     });
     return true;
   };
 
+  /* gets us a random question */
   this.randomQuestion = function(){
     //while(this.alreadyAnswered.indexOf(this.currentQuestion) != -1)
       this.currentQuestion = parseInt(Math.random() * bunsho.products.length);
     //this.alreadyAnswered.push(this.currentQueestion);
     this.userAnswer = '';
+    $scope.revisionWords = '';
     this.parseQuestion('en');
     return true;
   };
 
+  /* prepares the text of a question for display */
   this.parseQuestion = function(lang){
     var questionText = '';
 
     if(lang == 'en')
       questionText = bunsho.products[this.currentQuestion].sentence_english;
 
-    questionText = questionText.replace(/\[/g,'<span class="hint">');
+    questionText = questionText.replace(/\[/g,'<span class="hint" ng-click="question.showHint(0)">');
+
+    // replace with a for loop that assigns a number to each span
+
     questionText = questionText.replace(/\]/g,'</span>');
 
     this.redrawResults();
@@ -43,6 +55,23 @@ angular.module('bunsho').controller('QuestionController', ['$http', '$scope', fu
     $scope.parsedQuestion = questionText;
   };
 
+  /* grabs a hint and pushes it into the revision list */
+  this.grabHint = function(number){
+    var hint = this.products[this.currentQuestion].dict[number].word;
+
+    if(this.revisionList.indexOf(hint) == -1)
+      this.revisionList.push(hint);
+
+    return hint;
+  };
+
+  /* shows the hint on the page */
+  this.showHint = function(number){
+    var hint = this.grabHint(number);
+    $scope.revisionWords = 'このことばは<b>' + hint + '</b>です。';
+  };
+
+  /* redraws the result indicator on the top of the question page */
   this.redrawResults = function(){
     var tempResults = '';
     this.noCorrect = 0;
@@ -68,6 +97,7 @@ angular.module('bunsho').controller('QuestionController', ['$http', '$scope', fu
     $scope.resultsMeter = tempResults;
   };
 
+  /* checking procedure that is called at the submission of the form*/
   this.checkAnswer = function(){
     if(bunsho.products.length == 0) return false;
 
@@ -78,11 +108,13 @@ angular.module('bunsho').controller('QuestionController', ['$http', '$scope', fu
       return false;
   };
 
+  /* pushes an integer symbolizing a correct answer to the array */
   this.answerCorrect = function(){
     this.results.push(1);
     return true;
   };
 
+  /* pushes an integer symbolizing a incorrect answer to the array */
   this.answerIncorrect = function(){
     this.results.push(0);
     return true;
